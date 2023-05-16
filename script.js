68d8f9c5f90fd29a689d31b09d0d0fd40e30868e
Yonkonor <128113785+Yonkonor@users.noreply.github.com>
2023-05-16 09:32:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ document.addEventListener("DOMContentLoaded", function() {
   var button = document.getElementById("button");
   var valueBox = document.getElementById("valueBox");
 
-  var containerWidth = 500;
-  var buttonSize = 20;
+  var containerWidth = 700;
+  var buttonSize = 70;
 
-  button.addEventListener("mousedown", function(event) {
+  button.addEventListener("mousedown", startMovingButton);
+  button.addEventListener("touchstart", startMovingButton);
+
+  function startMovingButton(event) {
     event.preventDefault();
-    document.addEventListener("mousemove", moveButton);
-    document.addEventListener("mouseup", stopMovingButton);
-  });
+
+    if (event.type === "mousedown") {
+      document.addEventListener("mousemove", moveButton);
+      document.addEventListener("mouseup", stopMovingButton);
+    } else if (event.type === "touchstart") {
+      document.addEventListener("touchmove", moveButton);
+      document.addEventListener("touchend", stopMovingButton);
+    }
+  }
 
   function moveButton(event) {
-    var x = event.pageX - buttonSize / 2;
+    var x;
+
+    if (event.type === "mousemove") {
+      x = event.pageX - buttonSize / 2;
+    } else if (event.type === "touchmove") {
+      x = event.touches[0].pageX - buttonSize / 2;
+    }
+
     if (x < 0) {
       x = 0;
     } else if (x > containerWidth - buttonSize) {
@@ -27,9 +43,15 @@ document.addEventListener("DOMContentLoaded", function() {
     valueBox.innerHTML = XA.toFixed(2);
   }
 
-  function stopMovingButton() {
-    document.removeEventListener("mousemove", moveButton);
-    document.removeEventListener("mouseup", stopMovingButton);
+  function stopMovingButton(event) {
+    if (event.type === "mouseup") {
+      document.removeEventListener("mousemove", moveButton);
+      document.removeEventListener("mouseup", stopMovingButton);
+    } else if (event.type === "touchend") {
+      document.removeEventListener("touchmove", moveButton);
+      document.removeEventListener("touchend", stopMovingButton);
+    }
+
     showValueBox();
   }
 
@@ -37,3 +59,4 @@ document.addEventListener("DOMContentLoaded", function() {
     valueBox.style.display = "block";
   }
 });
+
